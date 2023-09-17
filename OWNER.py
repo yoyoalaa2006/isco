2672d236be988200c5d01938bc8bf65292453d45
yoyoalaa2006 <97169638+yoyoalaa2006@users.noreply.github.com>
2023-09-17 21:40:29 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Y_ALAA2"]
+OWNER_NAME = "- 𝗬𝗼𝗨𝘀𝗦𝗲𝗙 𝗤𝗮𝗬𝗲𝗗 𓏺 ."
+BOT_TOKEN = "6022370529:6381124680:AAEmVY_J9TBCVGuPhIhasNgrro6fYfsBBrE"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/pV_iSCo"
+GROUP = "https://t.me/Xu_Mz"
+VIDEO = "https://telegra.ph/file/db997a6c822692117e866.jpg"
+LOGS = "isco2"
+
